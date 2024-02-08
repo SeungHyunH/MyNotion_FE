090c923db96616f03bbdc2d25554f301af017c64
한승현 <vkflek1232@naver.com>
2024-02-08 13:11:19 +0900

    axios 추가

--- src/App.js
+++ src/App.js
@@ -1,71 +1,20 @@
 import React, { useCallback, useState } from "react";
-
+import axios from "axios";
 function App() {
-  const [id, setId] = useState("");
-  const [password, setPassword] = useState("");
-  const [token, setToken] = useState("");
-
-  const login = useCallback(() => {
-    fetch(`${process.env.REACT_APP_BACKEND}api/auth/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username: id,
-        password: password,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.code === 200) {
-          alert(data.message);
-          setToken(data.data);
-        } else {
-          throw data;
-        }
-      }) // 받은 토큰을 상태에 저장
-      .catch((err) => console.log(err));
-  }, [id, password]);
-
+  const [inputData, setInputData] = useState("");
   const test = useCallback(() => {
-    fetch(`${process.env.REACT_APP_BACKEND}api/hello`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((data) => {
-        console.log(data);
+    axios
+      .post(`${process.env.REACT_APP_BACKEND}api/v1/test`, {
+        data: inputData,
       })
-      .catch((error) => {
-        console.error("There was a problem with the fetch operation:", error);
-      });
-  }, [token]);
+      .then(({ data }) => console.log(data))
+      .catch((err) => console.log(err.response.data));
+  }, [inputData]);
 
   return (
     <div className="App">
-      <form>
-        <label>
-          아이디
-          <input autoComplete="username" onChange={(e) => setId(e.target.value)} />
-        </label>
-        <label>
-          비밀번호
-          <input
-            autoComplete="new-password"
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <button type="button" onClick={login}>
-          로그인
-        </button>
-      </form>
-      <button type="button" onClick={test}>
-        테스트
-      </button>
-      <div>토큰 : {token}</div>
+      <input type="text" onChange={(e) => setInputData(e.target.value)} />
+      <button onClick={test}>테스트</button>
     </div>
   );
 }
